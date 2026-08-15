a5801cb7fb815380d2d35246597cbc6b5de94994
alantango <alantango@gmail.com>
2026-08-15 18:36:22 -0400

    add test msg

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import datetime as dt
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return "<p>Hello form pythonAnywhere, time is now " + time + ".</p>"
 
 ## below only needed if you want to run the app directly:
 ##   python -m flask --app app run --debug
